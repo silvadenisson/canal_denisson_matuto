f7a5cd2d549750cbfd750c4a1632ed039792cea4
Denisson Silva <denisson@denissonsilva.com>
2021-07-15 17:03:58 -0300

    Create live02.R

---
+++ live02.R
@@ -0,0 +1,60 @@
+
+# verificando dados de vacinação
+# fontes de erros em possiveis análises
+
+# caso de exemplo vacinas aplicada em alagoas
+# fonte de dados opendataus
+# https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/ef3bd0b8-b605-474b-9ae5-c97390c197a8
+
+# limpando a área
+rm(list = ls())
+
+# carregando pacotes
+library(data.table)
+library(ggplot2)
+library(stringr)
+
+# carregando dados 
+vacinas <- fread("vacinas.csv", encoding = "Latin-1")
+
+str(vacinas)
+
+summary(vacinas$paciente_idade)
+
+View(vacinas[paciente_idade > 100, ])
+
+# "b5cfdb3a5deddf8921d8db85abc4e5c1b7c6e6a324b67675b8f5456298a78118"
+
+View(vacinas[paciente_idade < 18, ])
+# 9fd62e48d5eb3d9000a2fd6c2faf8204474925e47141ff18fd9fef8302002a7f
+
+
+#vericando casos de vacinacao duplicada e outros casos estranhos
+vac <- dcast(vacinas, paciente_id ~ vacina_descricao_dose)
+
+
+
+
+vacinas <- vacinas[paciente_idade > 17 & paciente_idade < 110, ]
+
+vacinas$vacinacao_completa <- ifelse(str_detect(vacinas$vacina_descricao_dose, "1"), 
+                                     "parcial",  "completa" )
+
+
+vacinas <- unique(vacinas[, c(2, 19, 28, 35)])
+
+vac_al <- vacinas[, .N, by = list(vacina_dataaplicacao, vacinacao_completa)]
+
+vac_al <- vac_al[order(vacina_dataaplicacao), ]
+
+vac_al <- vac_al[, acumulada := cumsum(N), by = vacinacao_completa ]
+
+options(scipen = 9999)
+
+ggplot(vac_al, aes(vacina_dataaplicacao, acumulada, color = vacinacao_completa)) + geom_line() +
+  theme_classic() +
+  scale_color_manual(name = "", values = c( "blue", "red")) +
+  labs(x = "", y = "N acumulado") +
+  scale_y_continuous(limits = c(0, 3000000)) +
+  geom_hline(yintercept = 2100000, linetype = 2)
+
