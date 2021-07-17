dbdf5da7be2df8fba441437eb49e434e5c7f165d
Denisson Silva <denisson@denissonsilva.com>
2021-07-17 16:21:12 -0300

    Update live02.R

--- live02.R
+++ live02.R
@@ -32,25 +32,29 @@ View(vacinas[paciente_idade < 18, ])
 #vericando casos de vacinacao duplicada e outros casos estranhos
 vac <- dcast(vacinas, paciente_id ~ vacina_descricao_dose)
 
-
-
-
+# selcionando casos onde a idade do paciente é maior qur 17 anos e menor que 110 amos
 vacinas <- vacinas[paciente_idade > 17 & paciente_idade < 110, ]
 
+# criando uma nova variavel que indica se vacinação completa ou parcial
 vacinas$vacinacao_completa <- ifelse(str_detect(vacinas$vacina_descricao_dose, "1"), 
                                      "parcial",  "completa" )
 
-
+# separando as variaveis de interesse pelo id da coluna
 vacinas <- unique(vacinas[, c(2, 19, 28, 35)])
 
+# agregando (somando/contando) a quantidade de vacinas aplicadas por dia e pelos status de vacinação parcial ou completa
 vac_al <- vacinas[, .N, by = list(vacina_dataaplicacao, vacinacao_completa)]
 
+# ordenando a base de dados pela data da vacina
 vac_al <- vac_al[order(vacina_dataaplicacao), ]
 
+# criando uma variavel com a soma acumulada dia a dia, pelo status
 vac_al <- vac_al[, acumulada := cumsum(N), by = vacinacao_completa ]
 
+# retirando a notação cientifica da exibição do resultados
 options(scipen = 9999)
 
+# criando um grafico da evolução da vacinacao em alagoas comparando pelo status de completa ou parcial
 ggplot(vac_al, aes(vacina_dataaplicacao, acumulada, color = vacinacao_completa)) + geom_line() +
   theme_classic() +
   scale_color_manual(name = "", values = c( "blue", "red")) +
